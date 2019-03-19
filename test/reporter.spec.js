95663d616bc35225e770e785c72ded55d004c400
rich.peters <rpeters@micro-magic.com>
2019-03-18 19:43:23 -0700

    almost have it tests running,  need to adjust the test data

--- test/reporter.spec.js
+++ test/reporter.spec.js
@@ -8,25 +8,35 @@ const Nightmare = require('nightmare');
 const nightmare = Nightmare({
   show: false,
 })
-// const moment = require('moment')
-// import {
-//     SUITE, COLORS, RESULTLIST, SUMMARY, ERRORS, ERRORLIST,
-//     STATS, STATS_WITH_NO_SPECS, SUITERESULT, JOBLINKRESULT,
-//     ERRORS_NO_STACK, ERRORLIST_NO_STACK, SUITES_SUMMARY,
-//     STATS_WITH_MULTIPLE_RUNNERS
-// } from './fixtures'
+
 const SUITE = require('./fixtures').SUITE
 const RESULTLIST = require('./fixtures').RESULTLIST;
 
 const HtmlReporter = require('../build/reporter') ;
 
- var htmlReporter = new HtmlReporter({
+ let htmlReporter = new HtmlReporter.default({
    debug: true,
    outputDir: './reports/html-results/',
    filename: 'report.html',
    reportTitle: 'Test Report Title',
    showInBrowser:true
  }) ;
+
+ //fill data filled by events
+ htmlReporter._events = {
+
+   };
+
+ htmlReporter.stats = {
+     runners: {
+
+     }
+   };
+  htmlReporter.reporters = [
+
+   ];
+
+
 describe('html reporter', () => {
   describe('the runner:start event', () => {
     it('should setup an initial state', () => {
@@ -51,14 +61,6 @@ describe('html reporter', () => {
     })
   })
 
-  describe('the test:pending event', () => {
-    it('should increase pending tests', () => {
-      htmlReporter.onTestPending({
-        cid: 42
-      })
-      htmlReporter.results[42].pending.should.equal(1)
-    })
-  })
 
   describe('the test:pass event', () => {
     it('should increase passing tests', () => {
@@ -103,14 +105,14 @@ describe('html reporter', () => {
                   'First Test': {
                     type: 'test',
                     title: 'The first test title',
-                    state: 'pass',
+                    state: 'passing',
                     screenshots: [],
                     logit: [],
                   },
                   'Second Test': {
                     type: 'test',
                     title: 'The second test title',
-                    state: 'fail',
+                    state: 'failing',
                     screenshots: [],
                     logit: [],
                   },
@@ -134,7 +136,7 @@ describe('html reporter', () => {
                   'First Test': {
                     type: 'test',
                     title: 'The first test title',
-                    state: 'pass',
+                    state: 'passing',
                     screenshots: [],
                     logit: [],
                   },
@@ -169,7 +171,7 @@ describe('html reporter', () => {
                   'First Test': {
                     type: 'test',
                     title: 'The first test title',
-                    state: 'pass',
+                    state: 'passing',
                     screenshots: [],
                     logit: [],
                   },
@@ -192,28 +194,25 @@ describe('html reporter', () => {
             },
           },
         },
-      }
+      };
 
 
 
-      htmlReporter.stats.runners['42'] = testrunner1
-      htmlReporter.stats.runners['43'] = testrunner2
+      htmlReporter.stats.runners['42'] = testrunner1;
+      htmlReporter.stats.runners['43'] = testrunner2;
       htmlReporter.stats.counts = {
         passes: 2,
         pending: 1,
         failures: 1
-      }
-      htmlReporter.onRunnerStart( testrunner1)
-      htmlReporter.onRunnerStart( testrunner2)
-      htmlReporter.onRunnerEnd({})
-
-      fs.existsSync('spec-report.html').should.eql(true)
-      sleep.sleep(1)
-
-      // fs.removeSync('spec-report.html')
+      };
+      htmlReporter.onRunnerStart( testrunner1);
+      htmlReporter.onRunnerStart( testrunner2);
+      htmlReporter.onRunnerEnd(htmlReporter.stats);
+      let reportfile = path.join(htmlReporter.options.outputDir, htmlReporter.options.filename);
+      fs.existsSync(reportfile).should.eql(true)
 
       nightmare
-        .goto(`file://${__dirname}/../spec-report.html`)
+        .goto(`file://${reportfile}`)
         // .type('#search_form_input_homepage', 'github nightmare')
         // .click('#search_button_homepage')
         // .wait('.result__title a')
